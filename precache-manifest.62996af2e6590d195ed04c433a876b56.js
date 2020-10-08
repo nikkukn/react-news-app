071cefea715385c3392421542d4eb5b2f4703487
nikkukn <raviraman9192@gmail.com>
2020-10-08 12:19:18 +0530

    Updates

---
+++ precache-manifest.62996af2e6590d195ed04c433a876b56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abc3860584bb858f32952a6b0f18d9b",
+    "url": "/github.io/react-news-app/index.html"
+  },
+  {
+    "revision": "9b83ff8e9721ebf0af5d",
+    "url": "/github.io/react-news-app/static/js/2.acbc081d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/github.io/react-news-app/static/js/2.acbc081d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b1757489e4c1c2008c",
+    "url": "/github.io/react-news-app/static/js/main.02b47b7d.chunk.js"
+  },
+  {
+    "revision": "a962cb093950c39863de",
+    "url": "/github.io/react-news-app/static/js/runtime-main.c9f5fa28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/react-news-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
